ad3df2aad80db8fd977d0cff013774e6a811acb1
Ejeruke Ewoma <ejerukeewoma@yahoo.com>
2022-07-02 04:29:10 -0700

    variable three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - entrt point
+*Return: always 0(success)
+*/
+int main(void)
+{
+int ch;
+fir (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
