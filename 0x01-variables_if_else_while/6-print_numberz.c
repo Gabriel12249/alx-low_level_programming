1751927e10432fa7f36a843e0b7ec0dca87bd479
Ejeruke Ewoma <ejerukeewoma@yahoo.com>
2022-07-02 06:04:33 -0700

    variable six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: always 0(success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
