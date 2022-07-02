97e8f3466ae370c418f5a776a6296a28a5ac155f
Ejeruke Ewoma <ejerukeewoma@yahoo.com>
2022-07-02 04:39:37 -0700

    variable four

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: always 0(success)
+*/
+int mqin(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if(ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
