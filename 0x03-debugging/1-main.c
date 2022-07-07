8c398b4171aaadd6b50625a46518fc4777d48b2d
Ejeruke Ewoma <ejerukeewoma@yahoo.com>
2022-07-07 06:33:00 -0700

    debugging one

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,13 +9,13 @@ int main(void)
 {
 int i;
 
-print("infinite loop incoming: (\n");
+print("infinite loop incoming :(\n");
 
 i = 0;
 /*
 *while (i < 10)
 *{
-*putchar(i);
+*               putchar(i);
 *}
 */
 printf("infinite loop avoided! \\o/\n");
